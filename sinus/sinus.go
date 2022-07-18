0061c1a81a14af50d198253d9db0701fc87ed535
Konstantin Bulusov <konstantinbulusov@yandex.ru>
2022-07-18 18:29:21 +0200

    My programm to calculate sin, cos, tg & ctg of angles.

---
+++ sinus/sinus.go
@@ -0,0 +1,21 @@
+package main
+  
+import (
+    "fmt"
+    "math"
+	"os"
+)
+  
+func main() {
+ 	var x float64
+	fmt.Print("Enter angle (degrees): ")
+	fmt.Fscan(os.Stdin, &x)
+	r := x * math.Pi / 180
+    sin, cos := math.Sincos(r)
+	tg := sin/cos
+	ctg :=cos/sin
+
+    
+    fmt.Printf("Sin = %.1f Cos = %.1f\n Tg = %.1f Ctg = %.1f", sin, cos, tg, ctg)
+    
+}
