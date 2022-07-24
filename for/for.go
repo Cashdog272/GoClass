171b9f07c1d820ba55c27199edba397488a58063
Konstantin Bulusov <konstantinbulusov@yandex.ru>
2022-07-24 15:17:44 +0200

    My homework with for loop

---
+++ for/for.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+ multiply := 1
+ for i := 1; i < 11; i ++ {
+  multiply *= i
+ }
+ subtraction := 1000
+ for i := 1; i < 11; i ++ {
+  subtraction -= i
+ }
+  addition := 0
+ for i := 1; i < 102; i = i + 2 {
+  addition += i
+ }
+ division := 1000000.0
+ for i := 2.0; i < 11; i ++ {
+  division /= i
+ }
+ fmt.Println(multiply, subtraction, addition,division)
+}
