9e235811a9cde6dd6434899b88cc71fdaf4d02e8
Konstantin Bulusov <konstantinbulusov@yandex.ru>
2022-07-30 18:09:24 +0200

    My programm for money insurance

---
+++ limit/limit.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	var money float64
+
+	fmt.Print("Your investment in roubles: ")
+	fmt.Fscan(os.Stdin, &money)
+
+	if money < 0 {
+		fmt.Println("Please, enter positive sum")
+
+		os.Exit(1)
+	}
+	
+	if money > 1400000 {
+		fmt.Println("Sorry, you have too much money to be insured")
+
+		os.Exit(2)
+	}
+
+	const rate = 1.05
+	const limit = 1400000.0
+
+	years := 0
+
+	for money < limit {
+		money *= rate
+		years++
+	}
+
+	years--
+	money /= rate
+
+	fmt.Printf("Years insured = %v\n Maximum money insured = %.1f", years, money)
+}
